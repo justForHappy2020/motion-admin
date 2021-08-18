533ccd24def6ba710be208f4128b11cd04bea308
2582266840 <19jhluo@stu.edu.cn>
2021-08-18 18:15:13 +0800

    ok

--- src/api/userManager.js
+++ src/api/userManager.js
@@ -1,16 +1,20 @@
 import request from '@/utils/request'
 
-export function getList(page,size,if_ban,starTime,endTime,searchStr) {
-  return request({
-    url: '/admin/userManagerList',
-    method: 'get',
-    params:{
-      page:page,
-      size:size,
-      ifban:if_ban,
-      starTime:starTime,
-      endTime:endTime,
-      searchStr:searchStr
-      }
-  })
+export function getList(page, size) {
+    return request({
+        url: '/admin/userManagerList',
+        method: 'get',
+        params: {
+            page: page,
+            size: size
+        }
+    })
+}
+
+export function saveUserdata(data) {
+    return request({
+        url: '/community/saveUserdata',
+        method: 'post',
+        data
+    })
 }
